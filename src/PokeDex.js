33b84a1beb987b7fe957e76f5fe1704f5965f98c
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-12-17 16:25:23 -0600

    add useAxios custom hook

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
-import { v4 as uuid } from "uuid";
-import axios from "axios";
-import PokemonSelect from "./PokemonSelect";
-import PokemonCard from "./PokemonCard";
-import "./PokeDex.css";
+import React from 'react';
+import { useAxios } from './hooks';
+import PokemonSelect from './PokemonSelect';
+import PokemonCard from './PokemonCard';
+import './PokeDex.css';
 
 /* Renders a list of pokemon cards.
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
-  };
+  let [pokemon, addPokemon] = useAxios('https://pokeapi.co/api/v2/pokemon/');
+
   return (
-    <div className="PokeDex">
-      <div className="PokeDex-buttons">
+    <div className='PokeDex'>
+      <div className='PokeDex-buttons'>
         <h3>Please select your pokemon:</h3>
         <PokemonSelect add={addPokemon} />
       </div>
-      <div className="PokeDex-card-area">
+      <div className='PokeDex-card-area'>
         {pokemon.map(cardData => (
           <PokemonCard
             key={cardData.id}
@@ -31,7 +25,7 @@ function PokeDex() {
             name={cardData.name}
             stats={cardData.stats.map(stat => ({
               value: stat.base_stat,
-              name: stat.stat.name
+              name: stat.stat.name,
             }))}
           />
         ))}
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import axios from 'axios';
 
 export const useFlip = (initialState = true) => {
   const [isFacingUp, setIsFacingUp] = useState(initialState);
@@ -7,4 +9,15 @@ export const useFlip = (initialState = true) => {
   };
 
   return [isFacingUp, flipCard];
+};
+
+export const useAxios = url => {
+  const [responses, setResponses] = useState([]);
+
+  const addResponseData = async (restOfUrl = '') => {
+    const response = await axios.get(`${url}${restOfUrl}`);
+    setResponses(s => [...s, { ...response.data, id: uuid() }]);
+  };
+
+  return [responses, addResponseData];
 };
